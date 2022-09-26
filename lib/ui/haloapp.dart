62e601f409341533f1546e8a7c76488d8006353a
Dian Prawira <dianprawira@sisfo.untan.ac.id>
2022-09-26 09:06:59 +0700

    Update haloapp.dart

--- lib/ui/haloapp.dart
+++ lib/ui/haloapp.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 
-class HaloApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Pertama",
       home: Scaffold(
-        appBar: AppBar(title: Text('HaloApp')),
-        body: Text("Halo Semuanya"),
+        appBar: AppBar(title: const Text('HaloApp')),
+        body: const Text("Halo Semuanya"),
       ),
     );
   }
